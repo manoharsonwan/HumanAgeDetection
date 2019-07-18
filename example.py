2f4df7fdd748e579ac8017d10b32aac111ed98bc
manohar <manohar@qwickbit.com>
2019-07-18 18:11:26 +0530

    removed 872 image

--- example.py
+++ example.py
@@ -15,7 +15,7 @@ import random
  ##############################
  # for git hub update 
  ###########################
-
+ 
 file_path = "/home/qwickbit/Documents/Palm_API/EDCC-Palmprint-Recognition/palmprint_data"
 #upload_img = os.path.join(file_path, "a_01.bmp")
 #TEST_A_02_PALMPRINT_IMAGE = os.path.join(TEST_PALMPRINT_DATA_DIR, "a_02.bmp")
